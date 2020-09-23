9793bf98ba4991184a339cde7cb72bb584c8d193
Finn Bacall <finn.bacall@manchester.ac.uk>
2020-09-23 16:42:16 +0100

    Category select

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,18 @@
 module CategoriesHelper
+  def category_options_for_select
+    options_for_select(traverse_cat_tree(Category.tree))
+  end
+
+  private
+
+  def traverse_cat_tree(tree, prefix = '')
+    opts = []
+
+    tree.each do |category, children|
+      opts << ["#{prefix}#{category.title}", category.id]
+      opts += traverse_cat_tree(children, prefix + '--')
+    end
+
+    opts
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,19 @@
 class Category < ApplicationRecord
   belongs_to :parent_category, class_name: 'Category', optional: true
-  has_many :child_categories, class_name: 'Category', inverse_of: :parent_category
+  has_many :child_categories, class_name: 'Category', foreign_key: :parent_category_id, inverse_of: :parent_category
   has_many :sources
+
+  def self.roots
+    where(parent_category_id: nil)
+  end
+
+  def self.tree(categories = Category.roots)
+    t = {}
+
+    categories.each do |category|
+      t[category] = tree(category.child_categories)
+    end
+
+    t
+  end
 end
